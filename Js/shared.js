8827066a79733ce107ce5ea0b42ad076e9733b2d
happilove <happinessnwafor98@gmail.com>
2022-11-23 13:39:28 -0300

    Testing to push

--- Js/shared.js
+++ Js/shared.js
@@ -1,6 +1,6 @@
 var backdrop = document.querySelector(".backdrop");
 var modal = document.querySelector(".modal");
-var modalNoButton = document.querySelector(".modal__action--negative");
+var modalNoButton = document.querySelector(".modal__action-negative");
 var selectPlanButtons = document.querySelectorAll(".plan button");
 var toggleButton = document.querySelector(".toggle-button");
 var mobileNav = document.querySelector(".mobile-nav")
@@ -9,9 +9,9 @@ var mobileNav = document.querySelector(".mobile-nav")
 
 //console.dir(backdrop);
 for (var i = 0; i < selectPlanButtons.length; i++) {
-    selectPlanButtons[i].addEventListener("click", function() {
-       // modal.style.display = "block";
-      //backdrop.style.display = "block";
+  selectPlanButtons[i].addEventListener("click", function() {
+        modal.style.display = "block";
+        backdrop.style.display = "block";
             
        modal.classList.add("open");
        backdrop.classList.add("open");
@@ -20,7 +20,8 @@ for (var i = 0; i < selectPlanButtons.length; i++) {
 
 backdrop.addEventListener("click", function() {
     //mobileNav.style.display = 'none';
-    mobileNav.classList.remove("open");
+    mobileNav.classList.remove("open")
+    //console.dir(backdrop.style['background-image']);
      closeModal();
 });
 
@@ -31,8 +32,8 @@ if (modalNoButton) {
 //modalNoButton.addEventListener("click", closeModal);
 
  function closeModal() {
-   // backdrop.style.display = "none";
-    //modal.style.display = "none";
+    backdrop.style.display = "none";
+    modal.style.display = "none";
     if (modal) {
     modal.classList.remove("open");
     }
